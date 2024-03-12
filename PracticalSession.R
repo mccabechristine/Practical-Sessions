da5ab44efd53245bb64210569899a4d164979a9b
Christine McCabe <christine.mccabe@atu.ie>
2024-03-12 14:18:55 +0000

    second commit

--- PracticalSession.R
+++ PracticalSession.R
@@ -1,13 +1,39 @@
 x <- c(3, 12,7)
 y <- c(4, 9, 11)
+x
+y
 z <- x+y
-p <- c(2)
-q <- c(50)
-x1 <- x + p
-y1 <- y + q
+z
+
+x1 <- x + 2
+x1
+y1 <- y + 2
+y1
+
 sum(x1)
 mean(y1)
-w <- c(2,7,NA, 20)
-sum(w)
-help(na.rm)
-v <- c()
+
+w <- c(1, 2, NA, 3, 4)
+sum(w, na.rm = TRUE)
+
+help(sum)
+
+typeof(w)
+typeof(x)
+
+x
+y
+x > y
+x < y
+x != y
+x == y
+
+x <- append(x, 12)
+x
+
+u <- c(5, 6, 7, 8, 9, 10)
+u
+
+u <- append(u, letters[1])
+u
+
